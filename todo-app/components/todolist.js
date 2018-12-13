ddc8f6fc951cc3412770bd5f5720c46b7b3f7f66
Pearl Law <pearllynnelaw@gmail.com>
2018-12-13 11:43:53 -0800

    Add toggle method that changes appstate on circle click #1

--- todo-app/components/todolist.js
+++ todo-app/components/todolist.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 export default class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isCompleted: false
+    }
+    this.toggleCircle = this.toggleCircle.bind(this)
+  }
+
+  toggleCircle() {
+    this.setState(prevState => ({
+      isCompleted: !prevState.isCompleted
+    }))
+  }
 
   render() {
+    const { isCompleted } = this.state
     return (
       <View style={styles.container}>
-        <TouchableOpacity>
-            <Text style={styles.circle}></Text>
+        <TouchableOpacity onPress={this.toggleCircle}>
+          <View style={[styles.circle, isCompleted ? styles.completeCircle : styles.incompleteCircle]}></View>
         </TouchableOpacity>
-        <Text style={styles.text}>Hi</Text>
+        <Text style={[styles.text, isCompleted ? styles.text : styles.completed]}>Hi</Text>
       </View>
     )
   }
@@ -24,12 +38,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '300'
   },
+  completed: {
+    textDecorationLine: 'line-through',
+    color: '#C0C0C0'
+  },
   circle: {
     height: 30,
     width: 30,
     borderRadius: 15,
-    borderColor: '#C0C0C0',
     borderWidth: 2,
     margin: 20
+  },
+  completeCircle: {
+    borderColor: '#C0C0C0'
+  },
+  incompleteCircle: {
+    backgroundColor: '#C0C0C0' 
   }
 })
