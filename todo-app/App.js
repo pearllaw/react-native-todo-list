484a8b106631e0cf43271260de6fd5ceccbd29d4
Pearl Law <pearllynnelaw@gmail.com>
2018-12-12 16:19:42 -0800

    Add todo list component with input bar #1

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, StatusBar } from 'react-native'
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions } from 'react-native'
 import { LinearGradient } from 'expo'
 
 export default class App extends React.Component {
+  // constructor(props) {
+  //   super(props)
+    
+  // }
+
   render() {
+    const {height, width} = Dimensions.get('window')
     return (
       <LinearGradient style={styles.container}
         colors={['#4568dc', '#b06ab3']}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Todo List</Text>
+        <View style={styles.list} width={width - 60}>
+          <View style={styles.inputBox}>
+            <TextInput style={styles.input} placeholder="Add todo" />
+          </View>
+        </View>
       </LinearGradient>
     )
   }
@@ -25,5 +36,19 @@ const styles = StyleSheet.create({
     fontSize: 40,
     marginTop: 50, 
     fontWeight: '500'
+  },
+  list: {
+    backgroundColor: '#fff',
+    flex: 0.9,
+    borderRadius: 10,
+    marginTop: 25
+  },
+  input: {
+    padding: 20,
+    fontSize: 24
+  },
+  inputBox: {
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#a9a9a9'
   }
 })
