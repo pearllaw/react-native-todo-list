ca80c01ba9159a7239f139064ecb9d9741d93674
Pearl Law <pearllynnelaw@gmail.com>
2018-12-17 18:12:18 -0800

    Refactor edit item method #1

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,17 +1,47 @@
 import React from 'react'
-import { StyleSheet, Text, StatusBar} from 'react-native'
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, ScrollView } from 'react-native'
 import { LinearGradient } from 'expo'
 import TodoList from './components/todolist'
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: '',
+      textValue: ''
+    }
+    this.newTodoItemController = this.newTodoItemController.bind(this)
+  }
+
+  newTodoItemController(textValue) {
+    this.setState({ newTodo: textValue })
+  }
+
   render() {
+    const {height, width} = Dimensions.get('window')
+    const {newTodo} = this.state
     return (
       <LinearGradient style={styles.container}
         colors={['#4568dc', '#b06ab3']}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Todo List</Text>
-        <TodoList />
+        <View style={styles.list} width={width - 60}>
+          <View style={styles.inputBox}>
+            <TextInput style={styles.input} 
+              placeholder="Add todo"
+              value={newTodo}
+              onChange={this.newTodoItemController}
+              onSubmitEditing={() => {
+                this.setState({ newTodo: '' })
+              }}
+              returnKeyType={'done'}
+              autoCorrect={true} />
+          </View>
+          <ScrollView>
+            <TodoList textValue={'todoItem'} />
+          </ScrollView>
+        </View>
       </LinearGradient>
     )
   }
@@ -28,5 +58,20 @@ const styles = StyleSheet.create({
     fontSize: 40,
     marginTop: 50, 
     fontWeight: '500'
+  },
+  list: {
+    backgroundColor: '#fff',
+    flex: 0.9,
+    borderRadius: 10,
+    marginTop: 25
+  },
+  input: {
+    padding: 20,
+    fontSize: 24,
+    fontWeight: '300'
+  },
+  inputBox: {
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#a9a9a9'
   }
 })
--- todo-app/components/todolist.js
+++ todo-app/components/todolist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Dimensions, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Dimensions, TextInput } from 'react-native'
 
 export default class TodoList extends Component {
   constructor(props) {
@@ -7,31 +7,12 @@ export default class TodoList extends Component {
     this.state = {
       isEditing: false,
       isCompleted: false,
-      todoItem: '',
-      list: []
+      todoValue: ''
     }
-    this.addTodo = this.addTodo.bind(this)
-    this.clearInput = this.clearInput.bind(this)
     this.toggleCheck = this.toggleCheck.bind(this)
     this.editItem = this.editItem.bind(this)
     this.finishEdit = this.finishEdit.bind(this)
     this.controlInput = this.controlInput.bind(this)
-  }
-
-  addTodo(todo) {
-    this.setState({ todoItem: todo })
-  }
-
-  clearInput() {
-    const { todoItem } = this.state
-    this.submitTodo(todoItem)
-    this.setState({ todoItem: '' })
-  }
-
-  submitTodo(newTodo) {
-    const newListArr = this.state.list.slice()
-    newListArr.push(newTodo) 
-    this.setState({ list: newListArr })
   }
 
   toggleCheck() {
@@ -41,9 +22,10 @@ export default class TodoList extends Component {
   }
 
   editItem() {
+    const { textValue } = this.props
     this.setState({ 
       isEditing: true,
-      todoItem
+      todoValue: textValue 
     })
   }
 
@@ -51,107 +33,67 @@ export default class TodoList extends Component {
     this.setState({ isEditing: false })
   }
 
-  controlInput(todo) {
-    this.setState({ todoItem: todo })
+  controlInput(textValue) {
+    this.setState({ todoValue: textValue })
   }
 
   render() {
-    const { isEditing, isCompleted, todoItem, list } = this.state
-    const {height, width} = Dimensions.get('window')
+    const { isEditing, isCompleted, todoValue } = this.state
     return (
-    <View>
-      <View style={styles.inputBox} width={width - 60}>
-        <TextInput style={styles.input}
-          placeholder="Add todo"
-          value={todoItem}
-          onChangeText={this.addTodo}
-          onSubmitEditing={this.clearInput}
-          returnKeyType={'done'}
-          autoCorrect={false} />
-      </View> 
-      <ScrollView>
-      <View style={styles.container} width={width - 60}>
-        {list.length > 0 
-          ? <View style={styles.todos}>
-            {list.map((todo, index) => (
-            <View style={styles.row}>
-              <View style={styles.todoItem}>
-              <TouchableOpacity onPress={this.toggleCheck}>
-                <Text style={styles.button} key={index}>{ isCompleted ? '✅' : '⬜' }</Text>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <TouchableOpacity onPress={this.toggleCheck}>
+            <Text style={styles.button}>{ isCompleted ? '✅' : '⬜' }</Text>
+          </TouchableOpacity>
+          {isEditing
+            ? <TextInput value={todoValue} 
+                style={[styles.text, isCompleted ? styles.completed : styles.text]}
+                multiline={true}
+                returnKeyType={'done'}
+                onBlur={this.finishEdit}
+                onChangeText={this.controlInput} />
+            : <Text style={[styles.text, isCompleted ? styles.completed : styles.text]}>
+            {this.props.textValue}
+          </Text>
+          }
+        </View>
+        <View style={styles.buttonContainer}>
+          {isEditing
+            ? <TouchableOpacity onPressOut={this.finishEdit}>
+                <View style={styles.button}>
+                  <Text>🆗</Text>
+                </View>
               </TouchableOpacity>
-                {/* // <TextInput value={todoItem}
-                // style={styles.text}
-                // multiline={true}
-                // returnKeyType={'done'}
-                // onBlur={this.finishEdit}
-                // onChangeText={this.controlInput} /> */}
-              <Text style={[styles.text, isCompleted ? styles.completed : styles.text]}
-                key={index}>
-                {todo}
-              </Text>
-              </View>
-              <View style={styles.buttonContainer} key={index}>
-                {isEditing
-                  ? <TouchableOpacity onPressOut={this.finishEdit}>
-                      <View style={styles.button}>
-                        <Text>🆗</Text>
-                      </View>
-                    </TouchableOpacity>
-                  : <TouchableOpacity onPressOut={this.editItem}>
-                      <View style={styles.button}>
-                        <Text>✏️</Text>
-                      </View>
-                    </TouchableOpacity>
-                }  
-                <TouchableOpacity>
-                  <View style={styles.button}>
-                    <Text>❌</Text>
-                  </View>
-                </TouchableOpacity> 
-              </View>
+            : <TouchableOpacity onPressOut={this.editItem}>
+                <View style={styles.button}>
+                  <Text>✏️</Text>
+                </View>
+              </TouchableOpacity>
+          }  
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text>❌</Text>
             </View>
-            ))}
-          </View>
-          : null
-        }
+          </TouchableOpacity>
+        </View>
       </View>
-      </ScrollView>
-    </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  inputBox: {
-    padding: 20,
-    marginTop: 25,
-    marginBottom: 25,
-    borderColor: '#a9a9a9',
-    borderWidth: 0.5,
-    borderRadius: 10,
-    backgroundColor: '#fff'
-  },
-  input: {
-    fontSize: 24,
-    fontWeight: '300'
-  },
   container: {
-    backgroundColor: '#fff',
-    flex: 0.95,
-    borderRadius: 10,
-    width: Dimensions.get('window').width - 50
-  },
-  todos: {
-    flexDirection: 'column',
+    width: Dimensions.get('window').width - 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomColor: '#a9a9a9',
+    borderBottomWidth: 0.5,
     padding: 10
   },
   row: {
     flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between'
-  },
-  todoItem: {
-    flexDirection: 'row'
+    alignItems: 'center',
   },
   text: {
     fontSize: 24,
