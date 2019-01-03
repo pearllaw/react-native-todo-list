ea21e7d95cacdc7ad92bb4138648eb707c75aebe
Pearl Law <pearllynnelaw@gmail.com>
2019-01-03 12:50:11 -0800

    Set up store #5

---
+++ todo-app/store.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers/reducers'
+import App from './App'
+
+const store = createStore(reducer)
+
+export default class todoApp extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  }
+}
